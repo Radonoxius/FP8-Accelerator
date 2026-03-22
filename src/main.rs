4b864a42ff1766f7a22e2bfab70b6e2f745f9f73
Radonoxius <Radonoxius@protonmail.com>
2026-03-22 22:14:30 +0530

    Added software subtraction samples as csv.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use soft_fp8::{Fp8, subtraction::subtract};
 
 fn main() {
-    Fp8::print_differs(
-        25.0,
-        |a, b| subtract(a, b),
-        |af, bf| af - bf
+    Fp8::print_as_csv(
+        subtract
     );
 }
