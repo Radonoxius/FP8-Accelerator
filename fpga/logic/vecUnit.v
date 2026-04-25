2e142e03a7079422af6a4d31f04a4230776db780
Radonoxius <Radonoxius@protonmail.com>
2026-04-25 17:00:21 +0530

    The End. (Well, Almost...)

--- fpga/logic/vecUnit.v
+++ fpga/logic/vecUnit.v
@@ -52,26 +52,10 @@ module vecUnit (
     opUnit2 u26(a26, b26, r26, global_opcode);
 	opUnit2 u27(a27, b27, r27, global_opcode);
 
-
-    wire [7:0] r30, r31, r32, r33, r34;
-
-    wire [7:0] a30 = bus[16 +: 8], b30 = bus[8 +: 8], c30 = bus[0 +: 8];
-    wire [7:0] a31 = bus[40 +: 8], b31 = bus[32 +: 8], c31 = bus[24 +: 8];
-    wire [7:0] a32 = bus[64 +: 8], b33 = bus[56 +: 8], c32 = bus[48 +: 8];
-    wire [7:0] a33 = bus[88 +: 8], b34 = bus[80 +: 8], c33 = bus[72 +: 8];
-    wire [7:0] a34 = bus[112 +: 8], b35 = bus[104 +: 8], c34 = bus[96 +: 8];
-
-    opUnit3 u30(a30, b30, c30, r30, global_opcode);
-    opUnit3 u31(a31, b31, c31, r31, global_opcode);
-    opUnit3 u32(a32, b32, c32, r32, global_opcode);
-    opUnit3 u33(a33, b33, c33, r33, global_opcode);
-    opUnit3 u34(a34, b34, c34, r34, global_opcode);
-
 	always@(*) begin
 		casex(global_opcode)
 			3'b1xx: resx = {64'd0, r27, r26, r25, r24, r23, r22, r21, r20};
 			3'b001: resx = {r115, r114, r113, r112, r111, r110, r19, r18, r17, r16, r15, r14, r13, r12, r11, r10};
-			3'b010: resx = {88'd0, r34, r33, r32, r31, r30};
 			default: resx = 128'd0;
 		endcase
 	end
--- vfp8-runner/src/main.rs
+++ vfp8-runner/src/main.rs
@@ -1,7 +1,7 @@
 use std::time::Instant;
 
-use soft_fp8::division::divide;
-use vfp8_driver::{FpReg, Vfp8Accelerator, Vfp8Operator, ops::DoubleOperandExpr};
+use soft_fp8::division::inverse_divide;
+use vfp8_driver::{FpReg, Vfp8Accelerator, Vfp8Operator, ops::SingleOperandExpr};
 use vfp8_runner::fill_randoms;
 
 fn main() {
@@ -16,22 +16,22 @@ fn main() {
     for i in 0..rx.capacity() {
         rx.push(
             [
-                divide(&ax[i][0].into(), &bx[i][0].into()).0.into(),
-                divide(&ax[i][1].into(), &bx[i][1].into()).0.into(),
-                divide(&ax[i][2].into(), &bx[i][2].into()).0.into(),
-                divide(&ax[i][3].into(), &bx[i][3].into()).0.into(),
-                divide(&ax[i][4].into(), &bx[i][4].into()).0.into(),
-                divide(&ax[i][5].into(), &bx[i][5].into()).0.into(),
-                divide(&ax[i][6].into(), &bx[i][6].into()).0.into(),
-                divide(&ax[i][7].into(), &bx[i][7].into()).0.into(),
-                0.into(),
-                0.into(),
-                0.into(),
-                0.into(),
-                0.into(),
-                0.into(),
-                0.into(),
-                0.into()
+                inverse_divide(&ax[i][0].into()).0.into(),
+                inverse_divide(&ax[i][1].into()).0.into(),
+                inverse_divide(&ax[i][2].into()).0.into(),
+                inverse_divide(&ax[i][3].into()).0.into(),
+                inverse_divide(&ax[i][4].into()).0.into(),
+                inverse_divide(&ax[i][5].into()).0.into(),
+                inverse_divide(&ax[i][6].into()).0.into(),
+                inverse_divide(&ax[i][7].into()).0.into(),
+                inverse_divide(&ax[i][8].into()).0.into(),
+                inverse_divide(&ax[i][9].into()).0.into(),
+                inverse_divide(&ax[i][10].into()).0.into(),
+                inverse_divide(&ax[i][11].into()).0.into(),
+                inverse_divide(&ax[i][12].into()).0.into(),
+                inverse_divide(&ax[i][13].into()).0.into(),
+                inverse_divide(&ax[i][14].into()).0.into(),
+                inverse_divide(&ax[i][15].into()).0.into(),
             ]
         );
     }
@@ -48,16 +48,16 @@ fn main() {
     let t1 = Instant::now();
     for i in 0..rx.capacity() {
         rx[i] = device.compute(
-            DoubleOperandExpr::construct(
-                Vfp8Operator::Divide,
-                ax[i][0].into(), bx[i][0].into(),
-                ax[i][1].into(), bx[i][1].into(),
-                ax[i][2].into(), bx[i][2].into(),
-                ax[i][3].into(), bx[i][3].into(),
-                ax[i][4].into(), bx[i][4].into(),
-                ax[i][5].into(), bx[i][5].into(),
-                ax[i][6].into(), bx[i][6].into(),
-                ax[i][7].into(), bx[i][7].into(),
+            SingleOperandExpr::construct(
+                Vfp8Operator::Inverse,
+                ax[i][0].into(), ax[i][1].into(),
+                ax[i][2].into(), ax[i][3].into(),
+                ax[i][4].into(), ax[i][5].into(),
+                ax[i][6].into(), ax[i][7].into(),
+                ax[i][8].into(), ax[i][9].into(),
+                ax[i][10].into(), ax[i][11].into(),
+                ax[i][12].into(), ax[i][13].into(),
+                ax[i][14].into(), ax[i][15].into(),
             ).unwrap()
         ).unwrap()
     }
