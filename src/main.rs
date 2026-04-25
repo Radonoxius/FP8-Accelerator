075bf7636fad222f553010c5aca29a7dec685ce5
Radonoxius <Radonoxius@protonmail.com>
2026-04-25 16:32:13 +0530

    Removed comments.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use soft_fp8::{Fp8, division::{divide, inverse_divide}};
+use soft_fp8::{Fp8, division::divide};
 
 fn main() {
-    Fp8::print_as_csv1(
-        inverse_divide
+    Fp8::print_as_csv2(
+        divide
     );
 }
